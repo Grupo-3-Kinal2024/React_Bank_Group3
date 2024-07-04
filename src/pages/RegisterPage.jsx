411c738cdf53f7876d24d478cf3748453b017bbf
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-07-03 23:16:46 -0600

    little change

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -13,7 +13,7 @@ const RegisterPage = () => {
         <>
             <Navbar />
             <div className="py-5 flex  items-center justify-center  content-center">
-                <div className="bg-gray-100 p-8 rounded-lg max-w-2xl w-full shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
+                <div className="bg-gray-100 p-8 rounded-lg max-w-xl w-full shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
                     <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Sign up</h2>
                     <p className="text-center text-gray-600 mb-6">Join the community today!</p>
                     <form onSubmit={handleSubmit(onSubmit)}>
