f85019de3dd3fd76c67e2f3af3317c171b7865e9
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-07-04 22:25:02 -0600

    service design

--- src/routes/main.routes.jsx
+++ src/routes/main.routes.jsx
@@ -2,11 +2,13 @@ import React, { lazy } from 'react';
 const Home = lazy(() => import('../pages/HomePage.jsx'));
 const Account = lazy(() => import('../pages/AccountPage.jsx'));
 const TransferAccount = lazy(() => import('../pages/TransferAccountPage.jsx'));
+const Service = lazy(() => import('../pages/ServicePage.jsx'));
 
 const mainRoutes = [
     { path: '/', element: <Home /> },
     { path: '/account', element: <Account />},
-    { path: '/transferAccount', element: <TransferAccount />}
+    { path: '/transferAccount', element: <TransferAccount />},
+    { path: '/service', element: <Service />}
 ]
 
 export default mainRoutes;
