a884ac8cbd45e9283acf9f451317115cd19580fe
David <darevalo-2022062@kinal.edu.gt>
2024-07-09 23:15:58 -0600

    resolved error

--- src/components/dashboard/RegisterPage.jsx
+++ src/components/dashboard/RegisterPage.jsx
@@ -150,7 +150,7 @@ const RegisterPage = () => {
             </div>
             <br />
             <br />
-            <br />
+                                
         </>
     );
 };
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,11 +1,10 @@
 import React, { lazy } from 'react'
 
 const Login = lazy(() => import('../pages/LoginPage'));
-const Register = lazy(() => import('../pages/RegisterPage'));
 
 const authRoutes = [
     { path: '/login', element: <Login /> },
-    { path: '/register', element: <Register /> }
+
 ]
 
 export default authRoutes;
